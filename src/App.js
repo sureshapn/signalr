63141c7ee4795024810249de51bb60e8c105b580
subhu <subhu@subhus-MacBook-Air.local>
2020-01-13 10:24:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { LogLevel, HubConnectionBuilder } from '@aspnet/signalr';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      imageUrl: '',
+      response: ''
+    }
+  }
+  componentDidMount() {
+    let connection = new HubConnectionBuilder()
+    .withUrl("https://testsig.azurewebsites.net/api")
+    .configureLogging(LogLevel.Information)
+    .build();
+ 
+connection.on("newMessage", ({text}) => {
+  const {response, imageUrl} = JSON.parse(text)
+    this.setState({response, imageUrl})
+});
+ 
+connection.start()
+    .then(() => connection.invoke("send", "Hello"));
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Smart IoT Solution</h3>
+      <div style={{minHeight: "120px", marginTop:"10px"}}>
+        <h5>Image</h5>
+        <img src={this.state.imageUrl} className="App-logo" alt="uploaded" /></div>
+      <div>
+
+      <div>
+        <h5>Output</h5>
+        </div>
+        <div style={{width: '50%', marginLeft: '25%'}}>
+          <p style={{overflowWrap: 'break-word'}}>
+          {this.state.response}
+          </p>
+        </div>
+      </div>
     </div>
   );
+  }
 }
 
 export default App;
